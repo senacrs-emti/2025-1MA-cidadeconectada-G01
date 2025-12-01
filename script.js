95649e68d8fc941802c129587900ef20c0d69f9a
Pedro Machado <pedrohsantosmachado@gmail.com>
2025-12-01 12:16:06 -0300

    Mudança geral
    Fiz uma mudança geral do código.

--- script.js
+++ script.js
@@ -8,3 +8,13 @@
       box.style.display = "block";
     }
   });
+
+  window.addEventListener("scroll", () => {
+    const navbar = document.getElementById("navbar");
+
+    if (window.scrollY > 5) {
+        navbar.classList.add("active");   // ativa animação
+    } else {
+        navbar.classList.remove("active"); // volta ao normal
+    }
+});

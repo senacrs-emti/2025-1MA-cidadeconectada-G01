3979abd9122ed6d603b41c779eb9999360d14957
Pedro Machado <pedrohsantosmachado@gmail.com>
2025-12-02 12:08:15 -0300

    Faltando espaço
    A Página tá faltando o espaço para a animação do NAV acontecer.

--- script.js
+++ script.js
@@ -12,9 +12,10 @@
   window.addEventListener("scroll", () => {
     const navbar = document.getElementById("navbar");
 
-    if (window.scrollY > 5) {
+    if (window.scrollY > 2) {
         navbar.classList.add("active");   // ativa animação
     } else {
         navbar.classList.remove("active"); // volta ao normal
     }
 });
+
